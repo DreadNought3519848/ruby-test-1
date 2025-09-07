c5bb81cf797e30190040c6f8e43f5931da9f27e1
DreadNought3519848 <shouya070904@yahoo.co.jp>
2025-09-07 19:52:02 +0000

    試製掲示板1号

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,22 @@
 class PostsController < ApplicationController
     def index
-        @posts = Post.all
+        @posts = Post.all.order(created_at: :desc)
     end
+    def new
+        @post = Post.new
+    end
+    def create
+        @post = Post.new(post_params)
+        if @post.save
+            redirect_to root_path, notice: "投稿しました!"
+        eles
+          render :new
+        end
+    end
+
+private
+
+  def post_params
+      params.require(:post).permit(:name, :content)
+  end
 end
